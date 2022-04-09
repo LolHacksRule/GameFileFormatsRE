0dc6db5b934f7b9d40d74e93cfd04407167d7d52
LolHacksRule <dantefmele@outlook.com>
2022-04-09 15:26:16 -0700

    Update XGS texture noesis

--- Exient/tex_ExientXGSEngine_XGSTexture_XGT_MOD.py
+++ Exient/tex_ExientXGSEngine_XGSTexture_XGT_MOD.py
@@ -1,8 +1,8 @@
 #Mod by LolHacksRule, Acewell for original script https://www.zenhax.com/download/file.php?id=8466
 #Added support for RGB565, RGBA4444, PVRTC4BPP, ETC2_RGB
-#TODO: Add code to detect swizzle
-#It's not possible to detect Wii U/legacy textures right away so I commented sections that are moddable
-
+#Added support for AB Star Wars Wii palletized textures (credits to Allen for original code [https://raw.githubusercontent.com/leeao/Noesis-Plugins/master/Textures/tex_AngryBirdsStarWars_Wii_xgt.py])
+#It's not possible to detect Wii U textures rightaway so I commented sections that are moddable
+#Add console checks to ensure decoding works properly
 from inc_noesis import *
 
 def registerNoesisTypes():
@@ -20,7 +20,8 @@ def noepyCheckType(data):
 def noepyLoadRGBA(data, texList):
     bs = NoeBitStream(data)
     bs.seek(0x4)
-    dataOffset = bs.readUShort()
+    dataOffset = bs.readByte()
+    platform = bs.readByte()
     headerSZ = bs.readUShort()
     numMips = bs.readUByte()
     flag1 = bs.readByte()
@@ -28,77 +29,335 @@ def noepyLoadRGBA(data, texList):
     flag3 = bs.readByte()
     imgFmt = bs.readUShort()
     print(hex(imgFmt)+ ": format")
-    UNKNOWN = bs.readShort()
+    numColors = bs.readShort()
     imgWidth = bs.readUShort()
     imgHeight = bs.readUShort()
     imgWidth2 = bs.readUShort()
     imgHeight2 = bs.readUShort()
-    bs.readInt()
-    datasize = bs.readUInt()
-    data = bs.readBytes(datasize) 
+    paletteSize = bs.readInt()
+    dataSize = bs.readInt()
+    if dataSize == 0:
+        print("Texture is empty!")
+        return 1
+    #data = bs.readBytes(dataSize)
+    if platform == 0x00:
+        print("Platform: Android/Win32 [how]");
+    if platform == 0x01:
+        print("Platform: PlayStation 3");
+    if platform == 0x02:
+        print("Platform: iOS");
+    if platform == 0x03:
+        print("Platform: Revolution/Nintendo Wii");
+    if platform == 0x04:
+        print("Platform: Xbox 360");
+    if platform == 0x05:
+        print("Platform: iOS_OLD");
+    if platform == 0x06:
+        print("Platform: CTR/Nintendo 3DS");
+    if platform == 0x09:
+        print("Platform: Android_OLD");
+    if platform == 0x0B:
+        print("Platform: caFe/Nintendo Wii U");
+    if platform == 0x0E:
+        print("Platform: Orbis/PlayStation 4");
+    if platform == 0x10:
+        print("Platform: WinPhone");
     #RGB565_ABSW_WII
     if imgFmt == 0x0:
         print("RGB565_Swizzled (WIP!)")
-        data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "b5 g6 r5")
-        texFmt = noesis.NOESISTEX_RGBA32
+        print("Cannot deswizzle currently!")
+        if platform == 0x03:
+            data = bs.readBytes(dataSize)
+            #data = untile(bs.readBytes(dataSize),imgWidth,imgHeight,1,4,16) #Attempt to deswizzle ({1,0},{2,0},{0,1},{0,2})
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "b5 g6 r5")
+            texFmt = noesis.NOESISTEX_RGBA32
+        elif platform == 0x06:
+            data = bs.readBytes(dataSize)
+            #data = untile(bs.readBytes(dataSize),imgWidth,imgHeight,1,4,16) #Attempt to deswizzle ({1,0},{0,1},{2,0},{0,2},{4,0},{0,4})
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "b5 g6 r5")
+            texFmt = noesis.NOESISTEX_RGBA32
+        else:
+            data = bs.readBytes(dataSize)
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "b5 g6 r5")
+            texFmt = noesis.NOESISTEX_RGBA32
     elif imgFmt == 0x1:
+        data = bs.readBytes(dataSize)
         print("RGB565")
         data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "b5 g6 r5")
         texFmt = noesis.NOESISTEX_RGBA32
     #RGB565
     elif imgFmt == 0x2:
-        print("RGBA4444")
-        data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "a4 b4 g4 r4")
-        texFmt = noesis.NOESISTEX_RGBA32
-    #RGBA4444
+        if platform == 0x06:
+            data = bs.readBytes(dataSize)
+            data = untile(bs.readBytes(dataSize),imgWidth,imgHeight,8,8,16) #Attempt to deswizzle ({1,0},{0,1},{2,0},{0,2},{4,0},{0,4})
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "a4 b4 g4 r4")
+            texFmt = noesis.NOESISTEX_RGBA32
+        else:
+            data = bs.readBytes(dataSize)
+            print("RGBA4444")
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "a4 b4 g4 r4")
+            texFmt = noesis.NOESISTEX_RGBA32
+    #RGBA4444_OR_RGBA8888
     elif imgFmt == 0x3:
-        print("RGBA4444 OR RGBA8888\nIf it doesn't look right, look at the below comment!")
-        data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "a4 b4 g4 r4") #COMMENT AND REPLACE WITH "r8 g8 b8 a8" TO WORK WITH WII U/LEGACY RGBA8888 TEXTURES
-        texFmt = noesis.NOESISTEX_RGBA32
+        if platform == 0x03: #RGBA8888 Paletted Wii texture
+            data = bs.readBytes(dataSize)
+            data = read32RGBA(data, imgWidth, imgHeight, 4,4)
+            texFmt = noesis.NOESISTEX_RGBA32
+        elif platform == 0x06:
+            data = bs.readBytes(dataSize)
+            #data = untile(bs.readBytes(dataSize),imgWidth,imgHeight,1,4,16) #Attempt to deswizzle ({1,0},{0,1},{2,0},{0,2},{4,0},{0,4})
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "a8 b8 g8 r8")
+            texFmt = noesis.NOESISTEX_RGBA32
+        elif platform == 0x0B: #RGBA8888 Wii U
+            data = bs.readBytes(dataSize)
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "r8 g8 b8 a8")
+            texFmt = noesis.NOESISTEX_RGBA32
+        elif platform == 0x0E: #ARGB8888 PS4
+            data = bs.readBytes(dataSize)
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "b8 g8 r8 a8")
+            texFmt = noesis.NOESISTEX_RGBA32
+        elif platform == 0x01:
+            print("RGBA8888\nIf it doesn't look right, look at the below comment!")
+            data = bs.readBytes(dataSize)
+            #data = untile(bs.readBytes(dataSize),imgWidth,imgHeight,1,4,16) #Attempt to deswizzle ({1,0},{0,1},{2,0},{0,2},{4,0},{0,4})
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "a8 r8 g8 b8")
+            texFmt = noesis.NOESISTEX_RGBA32
+        else:
+            data = bs.readBytes(dataSize)
+            print("RGBA4444 OR RGBA8888\nIf it doesn't look right, look at the below comment!")
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "a4 b4 g4 r4") #REPLACE WITH "r8 g8 b8 a8" TO WORK WITH WII U TEXTURES
+            texFmt = noesis.NOESISTEX_RGBA32
     #RGBA8888
     elif imgFmt == 0x4:
+        data = bs.readBytes(dataSize)
         print("RGBA8888")    
         data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "r8 g8 b8 a8")
         texFmt = noesis.NOESISTEX_RGBA32
     elif imgFmt == 0x5:    
-        print("UNKNOWN!")
-        #data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "l8 a8")
-        #texFmt = noesis.NOESISTEX_RGBA32
+        print("RGBA8888 Paletized")
+        palettes = readPalette(2,paletteSize,bs)
+        data = untile(bs.readBytes(dataSize),imgWidth,imgHeight,8,4,8)
+        textureData = convert8to32(data,palettes,imgWidth,imgHeight)
+        texList.append(NoeTexture(rapi.getInputName(), imgWidth, imgHeight, textureData, noesis.NOESISTEX_RGBA32))
         return 1
     #R8A8 ???
-    elif imgFmt == 0x8:    
-        print("L8A8 OR DXT1 (WIP!)\nIf it doesn't look right, look at the below comment!")
-        data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "l8 a8") #COMMENT THIS AND THE BELOW LINE AND REPLACE THE BELOW WITH "texFmt = noesis.NOESISTEX_DXT1" (No quotes) TO WORK WITH WII U TEXTURES
-        texFmt = noesis.NOESISTEX_RGBA32
-    elif imgFmt == 0x0d:    
-        print("LA88_BE (WIP!)")
-        data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "l8 a8") #TODO#
+    elif imgFmt == 0x8:
+        if platform == 0x00: #Android
+            print("LA88")
+            data = bs.readBytes(dataSize)
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "r8 a8") #TODO#
+            texFmt = noesis.NOESISTEX_RGBA32
+        elif platform == 0x0B or platform == 0x01: #DXT1 Wii U/PS3
+            print("DXT1")
+            data = bs.readBytes(dataSize)
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "r8 g8 b8 a8")
+            texFmt = noesis.NOESISTEX_DXT1
+        else:
+            data = bs.readBytes(dataSize)
+            print("L8A8 OR DXT1 (WIP!)\nIf it doesn't look right, look at the below comment!")
+            data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "l8 a8") #COMMENT THIS AND THE BELOW LINE AND REPLACE THE BELOW WITH "texFmt = noesis.NOESISTEX_DXT1" (No quotes) TO WORK WITH WII U TEXTURES#
+            texFmt = noesis.NOESISTEX_RGBA32
+    elif imgFmt == 0x0d:
+        data = bs.readBytes(dataSize)
+        print("LA88_BE (WIP! Red is not the color)")
+        if platform == 0x03:
+            data = untile(bs.readBytes(dataSize),imgWidth,imgHeight,8,4,8)
+        #elif platform == 0x01:
+            #data = untile(bs.readBytes(dataSize),imgWidth,imgHeight,8,8,8)
+        data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "r8 a8") #TODO#
         texFmt = noesis.NOESISTEX_RGBA32
     #DXT1
     elif imgFmt == 0x18:
+        data = bs.readBytes(dataSize)
         print("DXT1")
         texFmt = noesis.NOESISTEX_DXT1
+    #LA44
+    elif imgFmt == 0x19:
+        data = bs.readBytes(dataSize)
+        print("LA44")
+        data = rapi.imageDecodeRaw(data, imgWidth, imgHeight, "r4 a4")
+        texFmt = noesis.NOESISTEX_RGBA32
     #DXT5
     elif imgFmt == 0x1a:
+        data = bs.readBytes(dataSize)
         print("DXT5")
         texFmt = noesis.NOESISTEX_DXT5
     #PVRTC_4BPP_RGB_v3
     elif imgFmt == 0x1e:    
+        data = bs.readBytes(dataSize)
         print("PVRTC4BPP_RGB (WIP!)")
         data = rapi.imageDecodePVRTC(data, imgWidth, imgHeight, 4)
         texFmt = noesis.NOESISTEX_RGBA32
     elif imgFmt == 0x1f:
+        data = bs.readBytes(dataSize)
         print("PVRTC4BPP_RGBA (WIP!)")
         data = rapi.imageDecodePVRTC(data, imgWidth, imgHeight, 4)
         texFmt = noesis.NOESISTEX_RGBA32
     #ETC RGB
     elif imgFmt == 0x23:
+        data = bs.readBytes(dataSize)
         data = rapi.callExtensionMethod("etc_decoderaw32", data, imgWidth, imgHeight, "rgb")
         texFmt = noesis.NOESISTEX_RGBA32
         print("ETC")
     elif imgFmt == 0xFC:
+        data = bs.readBytes(dataSize)
         data = rapi.callExtensionMethod("etc_decoderaw32", data, imgWidth, imgHeight, "rgba")
         texFmt = noesis.NOESISTEX_RGBA32
         print("ETC2_RGB")
+    elif imgFmt == 0xFD: #3ds only?
+        data = bs.readBytes(dataSize)
+        data = rapi.callExtensionMethod("etc_decoderaw32", data, imgWidth, imgHeight, "rgba")
+        texFmt = noesis.NOESISTEX_RGBA32
+        print("ETC1_RGB_SPLITALPHA")
     texList.append(NoeTexture(rapi.getInputName(), imgWidth, imgHeight, data, texFmt))
     return 1
+    
+#added palette code and modified
+
+def read32RGBA(pixel,width,height,tile_w,tile_h):
+    real_width,real_height = getRealWH(width,height,tile_w,tile_h)
+    tileSize = tile_w * tile_h * 4
+    numTW = real_width // tile_w
+    numTH = real_height // tile_h
+    textureData = bytearray(real_width * real_height * 4)
+    for y in range(0,real_height,tile_h):            
+        for x in range(0,real_width,tile_w):                
+            tileY_ID = y // tile_h
+            tileX_ID = x // tile_w                
+            dataPtr = tileY_ID * numTW * tileSize + tileSize * tileX_ID
+            for ty in range(tile_h):
+                for tx in range(tile_w):
+                    dstIndex = (y + ty) * real_width*4 + (x + tx) * 4   
+                    srcIndex = dataPtr + ty * tile_w * 2 + tx * 2
+                    srcIndex2 = dataPtr + 32 + ty * tile_w * 2 + tx * 2
+                    textureData[dstIndex]   = pixel[srcIndex+1]     #R
+                    textureData[dstIndex+1] = pixel[srcIndex2]      #G
+                    textureData[dstIndex+2] = pixel[srcIndex2+1]    #B
+                    textureData[dstIndex+3] = pixel[srcIndex]       #A
+    cropData = crop(textureData,real_width,real_height,width,height,32)
+    return cropData
+
+def readPalette(palFormat,palSize,bs):
+    numColor = palSize // 2
+    if palFormat == 0:
+        palette = [ia8(bs.readUShort()) for i in range(numColor)]
+    elif palFormat == 1:
+        palette = [rgb565(bs.readUShort()) for i in range(numColor)]
+    elif palFormat == 2:
+        palette = [rgb5a3(bs.readUShort()) for i in range(numColor)]
+    elif palFormat == -1:
+        return 0
+    else:
+        print("unsupport palette format: ",palFormat)
+        return 0
+    return palette
+
+def convert4to32(c4pixel,palette,width,height):
+    c8pixel = bytearray(width*height)
+    for i in range(width*height//2):
+        index = c4pixel[i]
+        id1 = (index >> 4) & 0xf
+        id2 = index & 0xf
+        c8pixel[i*2] = id1
+        c8pixel[i*2+1] = id2  
+    RGBA32Data = bytearray(width * height * 4)
+    for i in range(width * height):
+        RGBA32Data[i * 4:(i + 1) * 4] = palette[c8pixel[i]]         
+    return RGBA32Data
+
+def convert8to32(c8pixel,palette,width,height):
+    RGBA32Data = bytearray(width * height * 4)
+    for i in range(width * height):
+        RGBA32Data[i * 4:(i + 1) * 4] = palette[c8pixel[i]]         
+    return RGBA32Data
+
+def convert4to8(c4pixel,real_width,real_height):
+    c8pixel = bytearray(real_width*real_height)
+    for i in range(real_width*real_height//2):
+        index = c4pixel[i]
+        id1 = (index >> 4) & 0xf
+        id2 = index & 0xf
+        c8pixel[i*2] = id1
+        c8pixel[i*2+1] = id2
+    return c8pixel
+
+def untile(pixel,width,height,tile_w,tile_h,bpp):
+    #bpp is bits per pixel
+    #tile_w is tile width
+    #tile_h is tile height
+    real_width,real_height = getRealWH(width,height,tile_w,tile_h)
+    numTileW = real_width // tile_w
+    numTileH = real_height // tile_h
+    lineSize = tile_w * bpp // 8
+    tileSize = tile_w * tile_h * bpp // 8
+    untileData = bytearray(real_width * real_height * bpp // 8)
+    for y in range(numTileH):            
+        for x in range(numTileW):                            
+            dataPtr = y * numTileW * tileSize + tileSize * x
+            for ty in range(tile_h):
+                curHeight = y * tile_h + ty
+                curWidth = x * tile_w
+                dstIndex = (curHeight * real_width + curWidth) * bpp // 8   
+                srcIndex = dataPtr + ty * lineSize
+                untileData[dstIndex:dstIndex+lineSize] = pixel[srcIndex:srcIndex+lineSize]    
+    cropData = crop(untileData,real_width,real_height,width,height,bpp)
+    return cropData   
+
+def crop(pixel,real_width,real_height,cropWidth,cropHeight,bpp):
+    #Note: this function can't apply to 4bpp
+    #for 4bpp: first convert 4bpp to 8bpp then run this function
+    #Because some 4bpp array sizes are not integers.
+    if real_width == cropWidth and real_height == cropHeight:
+        return pixel
+    minWidth = min(real_width,cropWidth)
+    minHeight = min(real_height,cropHeight)
+    length = cropWidth * cropHeight * bpp // 8 
+    outPixel = bytearray(length)    
+    dstLineSize = minWidth * bpp // 8
+    srcLineSize = real_width * bpp // 8
+
+    for y in range(minHeight):        
+        dstIndex = y * dstLineSize
+        srcIndex = y * srcLineSize
+        outPixel[dstIndex:dstIndex+dstLineSize] = pixel[srcIndex:srcIndex+dstLineSize]
+    return outPixel
+
+def getRealWH(width,height,tile_w,tile_h):
+    real_width = width
+    real_height = height
+    if (width%tile_w):
+        real_width = (width-(width%tile_w))+tile_w            
+    if (height%tile_h) :
+        real_height = (height-(height%tile_h))+tile_h
+    return real_width,real_height
+
+def ia8(rawPixel):
+    t = bytearray(4)
+    t[0] = rawPixel & 0xFF
+    t[1] = rawPixel & 0xFF
+    t[2] = rawPixel & 0xFF
+    t[3] = rawPixel >> 8
+    return t    
+
+def rgb565(rawPixel):
+    t = bytearray(4)
+    t[0] = (((rawPixel >> 11) & 0x1F) * 0xFF // 0x1F)
+    t[1] = (((rawPixel >> 5)  & 0x3F) * 0xFF // 0x3F)
+    t[2] = (((rawPixel >> 0)  & 0x1F) * 0xFF // 0x1F)
+    t[3] = 0xFF
+    return t       
+
+def rgb5a3(rawPixel):
+    t = bytearray(4)
+    if (rawPixel & 0x8000) :  # r5g5b5
+        t[0] = (((rawPixel >> 10) & 0x1F) * 0xFF // 0x1F)
+        t[1] = (((rawPixel >> 5)  & 0x1F) * 0xFF // 0x1F)
+        t[2] = (((rawPixel >> 0)  & 0x1F) * 0xFF // 0x1F)
+        t[3] = 0xFF
+    else:  # r4g4b4a3
+        t[0] = (((rawPixel >> 8)  & 0x0F) * 0xFF // 0x0F)
+        t[1] = (((rawPixel >> 4)  & 0x0F) * 0xFF // 0x0F)
+        t[2] = (((rawPixel >> 0)  & 0x0F) * 0xFF // 0x0F)
+        t[3] = (((rawPixel >> 12) & 0x07) * 0xFF // 0x07)
+    return t    
+#end
