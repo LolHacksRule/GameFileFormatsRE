eba397ea77681c2e97814da7f7c33f48ad3c7804
LolHacksRule <LHRHackingGuy@outlook.com>
2023-08-31 16:40:57 -0700

    More PXT formats supported

--- ZenStudios/PX/tex_ZenStudios_PX_PXTexture_PXT.py
+++ ZenStudios/PX/tex_ZenStudios_PX_PXTexture_PXT.py
@@ -1,3 +1,5 @@
+#TYSM https://forums.gameex.com/forums/topic/23953-pinball-fx2-audio-tables-missing/ for WIP DXT fixes
+
 from inc_noesis import *
 from addrlib import *
 
@@ -17,93 +19,250 @@ def noepyCheckType(texData):
     
 def noepyLoadRGBA(texData, texList):
     bs = NoeBitStream(texData)
-    bs.seek(0x8)
-    texFmt = bs.readInt()
-    texWidth = bs.readInt()
-    texHeight = bs.readInt()
-    mips = bs.readInt()
-    print(texWidth, "x", texHeight)
-    if texFmt != 0xE1 and texFmt != 0xF0 and texFmt != 352 and texFmt != 353 and texFmt != 368 and texFmt != 370:
-        texData = texData[0x18:] #Do this so we read textures at 0x18
-    if texFmt == 0x0160 or texFmt == 0x0161 or texFmt == 0x0170 or texFmt == 0x0172:
-        print("H")
-        texData = texData[0x1c:]
-    NinTex_dataSz = bs.readInt();
-    if texFmt == 0xE1 or texFmt == 0xF0:
-        WiiUTex_unk = bs.readInt();
-    texData = bs.readBytes(NinTex_dataSz)
+    bs.seek(0x4)
+    dataType = bs.readUInt()
+    texFmt = bs.readUInt()
+    texWidth = bs.readUInt()
+    texHeight = bs.readUInt()
+    if dataType != 0x12 and dataType != 0x18:
+        mips = bs.readUInt()
+    
+        #apply DXT hackery
+        if texFmt == 0x40 or texFmt == 0x42 or texFmt == 0x44 or texFmt == 0x48 or texFmt == 0x50 or texFmt == 0x52 or texFmt == 0x54 or texFmt == 0xF0 or texFmt == 0xF4 or texFmt == 0x140:
+            nr_blocks_first = (texWidth * texHeight) >> 4
+            print(str(nr_blocks_first))
+            
+            vblocks1 = bytearray(nr_blocks_first*4)
+            pblocks1 = bytearray(nr_blocks_first*4)
+            ablocks1 = bytearray(nr_blocks_first*8)
+            ablocks2 = bytearray(nr_blocks_first*8)
+            
+            if texFmt == 0x40 or texFmt == 0x50 or texFmt == 0xF0 or texFmt == 0x140: #DXT1
+                vblocks1 = bs.readBytes(nr_blocks_first*4)
+                skipmips(bs, nr_blocks_first, mips, 4)
+                pblocks1 = bs.readBytes(nr_blocks_first*4)
+                skipmips(bs, nr_blocks_first, mips, 4)
+        
+            if texFmt == 0x42 or texFmt == 0x44 or texFmt == 0x54 or texFmt == 0xF4: #DXT3
+                ablocks1 = bs.readBytes(nr_blocks_first*8)
+                skipmips(bs, nr_blocks_first, mips, 8)
+                vblocks1 = bs.readBytes(nr_blocks_first*4)
+                skipmips(bs, nr_blocks_first, mips, 4)
+                pblocks1 = bs.readBytes(nr_blocks_first*4)
+                skipmips(bs, nr_blocks_first, mips, 4)
+        
+            if texFmt == 0x48 or texFmt == 0x52 or texFmt == 0x64: #DXT5
+                ablocks1 = bs.readBytes(nr_blocks_first*8)
+                skipmips(bs, nr_blocks_first, mips, 8)
+                ablocks2 = bs.readBytes(nr_blocks_first*8)
+                skipmips(bs, nr_blocks_first, mips, 8)
+        
+            texData = bytearray(nr_blocks_first*16)
+        
+            for blk in range(0, nr_blocks_first):
+                if texFmt == 0x40 or texFmt == 0x50 or texFmt == 0xF0 or texFmt == 0x140: #DXT1
+                    bsize = 8
+                    texData[blk*bsize+0] = vblocks1[blk*4+0]
+                    texData[blk*bsize+1] = vblocks1[blk*4+1]
+                    texData[blk*bsize+2] = vblocks1[blk*4+2]
+                    texData[blk*bsize+3] = vblocks1[blk*4+3]
+                    texData[blk*bsize+4] = pblocks1[blk*4+0]
+                    texData[blk*bsize+5] = pblocks1[blk*4+1]
+                    texData[blk*bsize+6] = pblocks1[blk*4+2]
+                    texData[blk*bsize+7] = pblocks1[blk*4+3]
+                if texFmt == 0x42 or texFmt == 0x44 or texFmt == 0x54 or texFmt == 0xF4: #DXT3
+                    bsize = 16
+                    texData[blk*bsize+0] = ablocks1[blk*8+0]
+                    texData[blk*bsize+1] = ablocks1[blk*8+1]
+                    texData[blk*bsize+2] = ablocks1[blk*8+2]
+                    texData[blk*bsize+3] = ablocks1[blk*8+3]
+                    texData[blk*bsize+4] = ablocks1[blk*8+4]
+                    texData[blk*bsize+5] = ablocks1[blk*8+5]
+                    texData[blk*bsize+6] = ablocks1[blk*8+6]
+                    texData[blk*bsize+7] = ablocks1[blk*8+7]
+                    texData[blk*bsize+8] = vblocks1[blk*4+0]
+                    texData[blk*bsize+9] = vblocks1[blk*4+1]
+                    texData[blk*bsize+10] = vblocks1[blk*4+2]
+                    texData[blk*bsize+11] = vblocks1[blk*4+3]
+                    texData[blk*bsize+12] = pblocks1[blk*4+0]
+                    texData[blk*bsize+13] = pblocks1[blk*4+1]
+                    texData[blk*bsize+14] = pblocks1[blk*4+2]
+                    texData[blk*bsize+15] = pblocks1[blk*4+3]
+                if texFmt == 0x48 or texFmt == 0x52: #DXT5
+                    bsize = 16
+                    for a in range(0, 8):
+                        texData[blk*bsize+a] = -ablocks1[blk*8+a] + 128 & 0xFF
+                    for a in range(0, 8):
+                        texData[blk*bsize+a+8] = ablocks2[blk*8+a] + 128 & 0xFF
+        
+            #           texData[blk*bsize+0] = ablocks1[blk*8+0]
+            #           texData[blk*bsize+1] = ablocks1[blk*8+1]
+            #           texData[blk*bsize+2] = ablocks1[blk*8+2]
+            #           texData[blk*bsize+3] = ablocks1[blk*8+3]
+            #           texData[blk*bsize+4] = ablocks1[blk*8+4]
+            #           texData[blk*bsize+5] = ablocks1[blk*8+5]
+            #           texData[blk*bsize+6] = ablocks1[blk*8+6]
+            #           texData[blk*bsize+7] = ablocks1[blk*8+7]
+            #           texData[blk*bsize+8] = ablocks2[blk*8+0]
+            #           texData[blk*bsize+9] = ablocks2[blk*8+1]
+            #           texData[blk*bsize+10] = ablocks2[blk*8+7]
+            #           texData[blk*bsize+11] = ablocks2[blk*8+6]
+            #           texData[blk*bsize+12] = ablocks2[blk*8+5]
+            #           texData[blk*bsize+13] = ablocks2[blk*8+4]
+            #           texData[blk*bsize+14] = ablocks2[blk*8+3]
+            #           texData[blk*bsize+15] = ablocks2[blk*8+2]
+            
+            
+        elif texFmt != 0xE1 and texFmt != 0xF0 and texFmt != 352 and texFmt != 353 and texFmt != 368 and texFmt != 370:
+            texData = texData[0x18:] #Do this so we read textures at 0x18
+        else:
+            NinTex_dataSz = bs.readUInt();
+            if texFmt == 0xE1 or texFmt == 0xF0 or texFmt == 0xF4:
+                align = bs.readUInt(); #If we read this, the tex off is at 0x20, else 0x1C
+                #texData = texData[0x20:] #Do this so we read textures at 0x18
+            texData = bs.readBytes(NinTex_dataSz)
+    else:
+        print("Cubemap textures are WIP!")
+        texData = texData[0x14:]
     #bpp = surfaceGetBitsPerPixel(texFmt)
         
     #texData = texData[0x20:]
+    
+
+    print(texWidth, "x", texHeight)
+    
     #RGBA8888 texture
-    if texFmt == 0x14: #Mobile
+    if texFmt == 0x14 or texFmt == 0x18: #Mobile
         print("RGB888")
-        texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "r8 g8 b8")
-        texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x16:    
+        texFmt = noesis.NOESISTEX_RGB24
+    elif texFmt == 0x16:
         print("RGBA8888") #Mobile
-        texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "r8 g8 b8 a8")
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x17:    
+    elif texFmt == 0x17:
         print("RGBA8888 other one")
         texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "a8 r8 g8 b8")
         texFmt = noesis.NOESISTEX_RGBA32
-    #DXT?
-    elif texFmt == 0x40:    
-        print("Possibly DXT, TODO!")
-        #texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "r8 g8 b8 a8")
+    elif texFmt == 0x40 or texFmt == 0x140:  
+        print("DXT1\nWIP")
+        #texData = rapi.imageUntile360DXT(texData, texWidth, texHeight, 8) #Keeping this here
+        texFmt = noesis.NOESISTEX_DXT1
+    elif texFmt == 0x42:
+        print("DXT2\nWIP")
+        texFmt = noesis.NOESISTEX_DXT3
+    elif texFmt == 0x42:
+        print("DXT3\nWIP")
+        texFmt = noesis.NOESISTEX_DXT3
+    elif texFmt == 0x44:
+        print("DXT4\nWIP")
+        #texData = rapi.imageDecodeDXT(texData, texWidth, texHeight, noesis.FOURCC_BC3)
+        texFmt = noesis.NOESISTEX_DXT5
+    elif texFmt == 0x50:
+        print("Xbox 360 DXT1 Tiled\nWIP")
+        texData = rapi.swapEndianArray(texData, 2)
+        texFmt = noesis.NOESISTEX_DXT1
+    elif texFmt == 0x52:
+        print("Xbox 360 UNK Tiled\nWIP")
+        texData = rapi.swapEndianArray(texData, 2)
+        texFmt = noesis.NOESISTEX_DXT5
+    elif texFmt == 0x54:
+        print("Xbox 360 DXT4 Tiled\nWIP")
+        texData = rapi.swapEndianArray(texData, 2)
+        texData = rapi.imageDecodeDXT(texData, texWidth, texHeight, noesis.FOURCC_BC3)
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x44:    
-        print("Possibly BC4 DXT, TODO!")
-        texData = rapi.imageDecodeDXT(texData, texWidth, texHeight, noesis.FOURCC_BC4)
-        texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x54:    
-        print("Xbox 360 DXT Tiled\nUNKNOWN")
-        texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x82:    
+    #todo formats 0x60 and 0x64, seen in Vita
+    elif texFmt == 0x80 or texFmt == 0x82:
         print("PVRTC4, TODO!")
         texData = rapi.imageDecodePVRTC(texData, texWidth, texHeight, 4)
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x83:    
+    elif texFmt == 0x83:
         print("PVRTC4A, TODO!")
         texData = rapi.imageDecodePVRTC(texData, texWidth, texHeight, 4)
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x84:    
+    elif texFmt == 0x84:
         print("Xbox 360 DXT Tiled\nUNKNOWN")
         #texData = rapi.imageDecodePVRTC(texData, texWidth, texHeight, 4)
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0xC0:    
+    elif texFmt == 0x90:
+        print("CTR Swizzled RGB888\nCannot deswizzle currently!")
+        #texData = unswizzleCTR(texData, texWidth, texHeight, 16, 8, 4, 2)
+        #texData = rapi.imageFlipRGBA32(texData, texWidth, texHeight, 0, 1)
+        texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "r8 g8 b8")
+        texFmt = noesis.NOESISTEX_RGBA32
+    elif texFmt == 0x91:
+        print("CTR Swizzled RGBA8888")
+        texData = unswizzleCTR(texData, texWidth, texHeight, 32, 8, 4, 2)
+        texData = rapi.imageFlipRGBA32(texData, texWidth, texHeight, 0, 1)
+        texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "a8 r8 g8 b8")
+        texFmt = noesis.NOESISTEX_RGBA32
+    elif texFmt == 0x92:
+        print("CTR Swizzled ETC1")
+        #texData = unswizzleCTR(texData, texWidth, texHeight, 32, 8, 4, 4)
+        texData = rapi.imageDecodePICA200ETC1(texData, texWidth, texHeight, 0)
+        texData = rapi.imageFlipRGBA32(texData, texWidth, texHeight, 0, 1)
+        texFmt = noesis.NOESISTEX_RGBA32
+    elif texFmt == 0x93:
+        print("CTR Swizzled ETC1A4")
+        texData = rapi.imageDecodePICA200ETC1(texData, texWidth, texHeight, 1)
+        texData = rapi.imageFlipRGBA32(texData, texWidth, texHeight, 0, 1)
+        texFmt = noesis.NOESISTEX_RGBA32
+    elif texFmt == 0xC0:
         print("PS Vita Swizzled RGBA8888\nDeswizzling is WIP!")
         texData = rapi.imageFromMortonOrder(texData, texWidth, texHeight)
         texData = rapi.imageFlipRGBA32(texData, texWidth, texHeight, 1, 0)
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x8A:    
+    elif texFmt == 0x8A:
         print("ETC") #this byte is inconsistent
         texData = rapi.callExtensionMethod("etc_decoderaw32", texData, texWidth, texHeight, "rgb")
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0xE1 or texFmt == 0xE2 :    
-        #texData = deswizzle(texWidth, texHeight, 1, texFmt, 0, 1, 4, 4, 256, 32, 0, 0, texData)
-        print("WiiU Swizzled RGBA8888\nCannot deswizzle currently!")
-        texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "r8 g8 b8 a8")
+    elif texFmt == 0x8D:
+        print("ETC1A4")
+        texData = rapi.imageDecodeETC(texData, texWidth, texHeight, "RGB")
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x0160 or texFmt == 0x0161:
-        #block_height = min(max(texHeight // 8, 1), 16)
-        #texData = unswizzleNX(texData, texWidth, texHeight, 4, block_height)
-        print("Switch Swizzled RGBA8888\nCannot deswizzle currently!")
-        texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "r8 g8 b8 a8")
+    elif texFmt == 0xE0:
+        print("WiiU Swizzled RGBX8888\WIP!")
+        texData = deswizzle(texWidth, texHeight, 1, texFmt, 0, 1, 4, 4, texWidth, 32, 0, 0, texData)
+        texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "r8 g8 b8 x8")
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x0170:    
-        #block_height = min(max(texHeight // 8, 1), 8)
-        #texData = unswizzleNX(texData, texWidth, texHeight, 2, block_height)
-        print("Switch Swizzled DXT1\nCannot deswizzle currently!")
-        texData = rapi.imageDecodeDXT(texData, texWidth, texHeight, noesis.NOESISTEX_DXT1)
+    elif texFmt == 0xE1:
+        print("WiiU Swizzled RGBA8888")
+        texData = deswizzle(texWidth, texHeight, 1, texFmt, 0, 1, 4, 4, texWidth, 32, 0, 0, texData)
         texFmt = noesis.NOESISTEX_RGBA32
-    elif texFmt == 0x0172:    
-        #block_height = min(max(texHeight // 8, 1), 8)
-        #texData = unswizzleNX(texData, texWidth, texHeight, 4, block_height)
-        print("Switch Swizzled DXT5\nCannot deswizzle currently!")
-        texData = rapi.imageDecodeDXT(texData, texWidth, texHeight, noesis.NOESISTEX_DXT5)
+    elif texFmt == 0xF0:
+        print("WiiU Swizzled BC1\nCannot deswizzle currently!")
+        #texData = deswizzle(texWidth, texHeight, 1, texFmt, 0, 1, 4, 4, texWidth, 32, 0, 0, texData)
+        texFmt = noesis.NOESISTEX_DXT1
+    elif texFmt == 0xF4:
+        #def deswizzle(width, height, depth, format_, aa, use, tileMode, swizzle_, pitch, bpp, slice, sample, data):
+        print("WiiU Swizzled BC3\nCannot deswizzle currently!")
+        #texData = deswizzle(texWidth, texHeight, 1, texFmt, 0, 1, 1, 1, texWidth, 32, 0, 0, texData)
+        texData = rapi.imageDecodeDXT(texData, texWidth, texHeight, noesis.FOURCC_BC3)
         texFmt = noesis.NOESISTEX_RGBA32
+    elif texFmt == 0x0148:
+        print("BC5S\nCannot deswizzle currently!")
+        #texData = rapi.callExtensionMethod("untile_1dthin", texData, texWidth, texHeight, 8, 1)
+        texData = rapi.imageDecodeDXT(texData, texWidth, texHeight, noesis.FOURCC_ATI2, 0.0, 2) #Reads BC5S
+        texFmt = noesis.NOESISTEX_RGBA32
+    elif texFmt == 0x0160:
+        print("NX Swizzled RGBA8888 SRGB\nCannot deswizzle currently!")
+        texData = rapi.imageDecodeRaw(texData, texWidth, texHeight, "b8 g8 r8 a8")
+        texFmt = noesis.NOESISTEX_RGBA32
+    elif texFmt == 0x0161:
+        print("NX Swizzled RGBA8888\nCannot deswizzle currently!")
+        texFmt = noesis.NOESISTEX_RGBA32
+    elif texFmt == 0x0170:
+        print("NX Swizzled DXT1\nCannot deswizzle currently!")
+        texFmt = noesis.NOESISTEX_DXT1
+    elif texFmt == 0x0171:
+        print("NX Swizzled DXT3\nCannot deswizzle currently!")
+        texFmt = noesis.NOESISTEX_DXT3
+    elif texFmt == 0x0172:
+        print("NX Swizzled DXT5\nCannot deswizzle currently!")
+        texFmt = noesis.NOESISTEX_DXT5
+    elif texFmt == 0x0178:
+        texData = rapi.imageDecodeDXT(texData, texWidth, texHeight, noesis.FOURCC_ATI2, 0.0, 2)
+        print("NX Swizzled BC5S\nCannot decode currently!")
+        texFmt = noesis.NOESISTEX_RGBA32
+        #return 1
     else:
         print(texFmt)
         print("UNKNOWN TEXTURE FORMAT DETECTED!")
@@ -111,40 +270,30 @@ def noepyLoadRGBA(texData, texList):
     texList.append(NoeTexture(rapi.getInputName(), texWidth, texHeight, texData, texFmt))
     return 1
 
-# adaptation of Yuzu Nintendo Switch Emulator (https://github.com/yuzu-emu/yuzu) unswizzling by https://github.com/orgs/yuzu-emu/people
-# https://git.metaa.io/mirror/yuzu/blob/a3e82e8e1f5cb39246f30cac045db8e243f0daee/src/video_core/textures/decoders.cpp
-# original source: Tegra K1 Technical Reference Manual, p.1187-1188 (https://developer.nvidia.com/embedded/tegra-k1-reference)
-# assume GPLv2+ License: https://github.com/yuzu-emu/yuzu/blob/master/license.txt
+def unswizzleCTR(buffer, width, height, bpp, l, m, s):
+    stripSize = bpp * s // 8
 
-def unswizzleNX(buffer, width, height, bpb, block_height=16, width_pad=16, height_pad=16):
-    # setup
-    out = bytearray(len(buffer))
-    if width % width_pad or height % height_pad:
-        width_show = width
-        height_show = height
-        width = width_real = ((width + width_pad - 1) // width_pad) * width_pad
-        height = height_real = ((height + height_pad - 1) // height_pad) * height_pad
-    else:
-        width_show = width_real = width
-        height_show = height_real = height
-    image_width_in_gobs = width * bpb // 64
+    result = bytearray(width * height * bpp // 8)
+    ptr = 0
 
-    # unswizzling
-    for Y in range(height):
-        for X in range(width):
-            Z = Y * width + X
-            GOB_address = 0 + (Y // (8 * block_height)) * 512 * block_height * image_width_in_gobs + (X * bpb // 64) * 512 * block_height + (Y % (8 * block_height) // 8) * 512
-            X *= bpb
-            address = GOB_address + ((X % 64) // 32) * 256 + ((Y % 8) // 2) * 64 + ((X % 32) // 16) * 32 + (Y % 2) * 16 + (X % 16)
-            out[Z * bpb:(Z + 1) * bpb] = buffer[address:address + bpb]
+    for y in range(0, height, l):
+        for x in range(0, width, l):
+            for y1 in range(0, l, m):
+                for x1 in range(0, l, m):
+                    for y2 in range(0, m, s):
+                        for x2 in range(0, m, s):
+                            for y3 in range(s):
+                                idx = (((y + y1 + y2 + y3) * width) + x + x1 + x2) * bpp // 8
+                                result[idx : idx+stripSize] = buffer[ptr : ptr+stripSize]
+                                ptr += stripSize
 
-    # crop
-    if width_show != width_real or height_show != height_real:
-        crop = bytearray(width_show * height_show * bpb)
-        for Y in range(height_show):
-            OffsetIn = Y * width_real * bpb
-            OffsetOut = Y * width_show * bpb
-            crop[OffsetOut:OffsetOut + width_show * bpb] = out[OffsetIn:OffsetIn + width_show * bpb]
-        out = crop
+    return result
 
-    return out
+#add fixes for DXT
+def skipmips(bsi, nr_blocks, mips, bsize):
+    for m in range(mips-1):
+        nr_blocks = nr_blocks >> 2
+        if nr_blocks <= 1:
+            nr_blocks = 1
+        bsi.seek(nr_blocks*bsize, NOESEEK_REL)
+    return 1
